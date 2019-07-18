85d38f9398a3e500c19220ecab8d4d9e9ac5df4b
Maryam <maryam.aslam@outlook.com>
2019-07-18 14:48:58 +0200

    Initial commit

---
+++ src/AssertionsExample.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AssertionsExample {
+
+    WebDriver driver;
+    @BeforeClass
+    void setup()
+    {
+        System.setProperty("webdriver.chrome.driver","E:/chromedriver_win32/chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.get("http://www.northernlightsaid.org");
+    }
+    @Test
+    void logoTest()
+    {
+        WebElement logo=driver.findElement(By.xpath("//*[@id=\"menuzord-right\"]/a[2]/img"));
+        Assert.assertTrue(logo.isDisplayed(),"Logo is not displayed");
+
+    }
+    @Test
+    void homePageTitle(){
+        String title = driver.getTitle();
+        Assert.assertEquals("Northern Lights Aid (NLA)",title,"Title is not matched");
+
+    }
+    @AfterClass
+    void tearDown(){
+        driver.quit();
+    }
+
+
+
+
+}
